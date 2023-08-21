008f42dcbb9d4af944359fb494f4e98b39737887
Izabelle ferreira da silva <127629684+izabelle20@users.noreply.github.com>
2023-08-21 15:22:30 -0300

    Update questão 1.cpp

--- questão 1.cpp
+++ questão 1.cpp
@@ -1,4 +1,4 @@
-/*Quest„o 1 - Crie uma struct chamada "Pessoa" que contenha os seguintes campos: nome, idade e altura. Em seguida, escreva um programa que declare uma vari·vel do tipo Pessoa,
+/*Quest√£o 1 - Crie uma struct chamada "Pessoa" que contenha os seguintes campos: nome, idade e altura. Em seguida, escreva um programa que declare uma vari√°vel do tipo Pessoa,
 preencha seus campos e imprima os valores..*/
 
 #include <stdio.h>
@@ -27,12 +27,20 @@ void SetPessoa(Pessoa *P, char nome[], int idade, float altura)
 
 int main()
 {
-    printf("\n");
+	printf("-----------------------------------------\n");
+	printf("Universidade Catolica de Brasilia\n");
+	printf("Aluna: Izabelle Ferreira da silva\n");
+	printf("Turma: GPE02M0411 - Esrrutura de dados");
+	printf("\n---------------------------------------\n");
+	printf("Bem-vindos ao sistema integrado a pessoa!\n");
+	printf("-----------------------------------------\n");
 
     Pessoa Izabelle;
     SetPessoa(&Izabelle, "Izabelle", 21, 1.50);
 
     ImprimePessoa(Izabelle);
 
+	printf("-----------------------------------------\n");
+	printf("Fim!\n");
     return 0;
 }
